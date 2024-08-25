0b09ba53491bb2a28c2aeadfcf2134dc59584c12
AslyB <asbarahonam@gmail.com>
2024-08-25 16:25:10 -0600

    perimetro y area del rectangulo

--- cuadradro.asm
+++ cuadradro.asm
@@ -5,8 +5,8 @@
     msj1 db 'El perímetro es de: $'
     msj2 db 'El área es de: $'
     
-    num1 dd 78536 ; FALTA LA FUNCION PARA QUE ESTOS VALORES SEAN LAS ENTREDAS (PASAR DE ASCII A NUM)  
-           
+    num1 dd 10 ; FALTA LA FUNCION PARA QUE ESTOS VALORES SEAN LAS ENTREDAS (PASAR DE ASCII A NUM)  
+    num2 dd 5     
            
         
     num1ResD dd ? ; dw = 4 byte   
@@ -14,8 +14,6 @@
     num2ResL dd ? ; para formar una respuesta de 8 bytes
 
    
-
-
 .code ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 main proc 
@@ -25,13 +23,16 @@ main proc
     mov ds, ax  
     
     ;call perimetroCuadrado
-    call areaCuadradro
-    
+    ;call areaCuadradro 
+    ;call perimetroRect 
+    call areaRect    
     ; Terminar el programa
     mov ah, 4Ch
     int 21h
      
-    main endp  
+    main endp
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 perimetroCuadrado proc ; num1 = lado   
      
@@ -117,8 +118,103 @@ areaCuadradro proc ; num1 = lado
     ; FALTA LA FUNCION PARA PASAR DE NUMERO A ASCII
          
     ret
-    areaCuadradro endp
+    areaCuadradro endp 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+perimetroRect proc ; num1= base, num2= altura
+    
+    ;parte baja base + parte baja altura  (b+h)
+    mov ax, word ptr [num1]
+    mov bx, word ptr [num2]
+    xor cx, cx ; preparar para el acarreo 
+    add ax, bx 
+    ;manejar el acarreo. ADC (Add with Carry): suma dos operandos junto con el valor del flag de acarreo (CF).
+    adc cx, 0       ; CX = 1 si CF = 1, CX = 0 si CF = 0
+    mov [num1ResD], ax  ;parte baja 
+    mov [num1ResD+2], cx  ;parte baja   
+    
+    ;parte alta base + parte alta altura  (b+h) 
+    mov ax, word ptr [num1+2]
+    mov cx, word ptr [num2+2]
+    add ax, cx    
+    add [num1ResD+2],ax  ;parte alta 
+    
+    ;parte baja * 2;  2*(b+h)
+    mov ax, word ptr [num1ResD]
+    mov cx, 2
+    mul cx ; queda en DX:AX 
+    mov [num2ResH+2], ax  
+    mov [num2ResH], dx  
+    
+    ;parte alta * 2;  2*(b+h)
+    mov ax, word ptr [num1ResD+2]
+    mov cx, 2
+    mul cx ; queda solo en AX 
+    add [num2ResH], ax 
+    
+    mov ax, [num2ResH] ;QUITAR DESPUES, solo es para verificar
+    mov bx, [num2ResH+2] ;QUITAR DESPUES, solo es para verificar
+      
+    ; Imprimir el mensaje del area
+    lea dx, msj2
+    call imprimir
+    
+    ;Imprimir el area calculada
+    ; FALTA LA FUNCION PARA PASAR DE NUMERO A ASCII
+      
+    ret
+    perimetroRect endp  
+
+areaRect proc ; num1= base, num2= altura 
+             
+    ;parte baja * parte baja
+    mov ax, word ptr [num1]
+    mov cx, word ptr [num2]
+    mul cx ; queda en DX:AX 
+    add [num2ResL+2], ax
+    add [num2ResL], dx
+   
+    ;parte alta * parte baja 
+    mov ax, word ptr [num1+2]
+    mov cx, word ptr [num2]
+    mul cx ; result solo queda en AX  
+    add [num2ResL], ax
+    add [num2ResH+2], dx  
+    
+    ;parte baja * parte alta
+    mov ax, word ptr [num1]
+    mov cx, word ptr [num2+2]
+    mul cx ; queda en DX:AX
+    xor cx, cx ; preparar para el acarreo 
+    add [num2ResL], ax 
+    ;manejar el acarreo. ADC (Add with Carry): suma dos operandos junto con el valor del flag de acarreo (CF).
+    adc cx, 0       ; CX = 1 si CF = 1, CX = 0 si CF = 0
+    add [num2ResH+2], cx
+    add [num2ResH+2], dx
+   
+    ;parte alta * parte alta
+    mov ax, word ptr [num1+2]
+    mov cx, word ptr [num2+2]
+    mul cx ; result solo queda en AX  
+    add [num2ResH+2], ax
+    add [num2ResH], dx 
+    
+    mov ax, [num2ResH] ;QUITAR DESPUES, solo es para verificar
+    mov bx, [num2ResH+2] ;QUITAR DESPUES, solo es para verificar
+    mov cx, [num2ResL] ;QUITAR DESPUES, solo es para verificar
+    mov dx, [num2ResL+2] ;QUITAR DESPUES, solo es para verificar   
+    
+    ; Imprimir el mensaje del area
+    lea dx, msj2
+    call imprimir
+    
+    ;Imprimir el area calculada
+    ; FALTA LA FUNCION PARA PASAR DE NUMERO A ASCII
+     
+    ret
+    areaRect endp
+    
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 ; PROCEDIMIENTOS GENERALES 
